29647090e9285af363c94e55c6b0b5e20eab9089
craig-mini <cm.nicol@outlook.com>
2020-10-13 17:32:02 +0100

    ExpenseSerializer added

--- budget_api_app/views.py
+++ budget_api_app/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.request import Request
+from rest_framework.response import Response
 
 # Create your views here.
+
+# class AddExpense(APIView):
+#     """
+#     Endpoint to add a new expense to the database.
+#     """
+#     def get(self, request: Request) -> Response:
+
+
